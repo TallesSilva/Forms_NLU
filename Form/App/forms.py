2e2476487adb1a1b203090ccbbe99e9cd9f1dba6
TallesSilva <tallesbugs@hotmail.com>
2019-12-03 12:31:36 -0300

    até aqui ok

--- Form/App/forms.py
+++ Form/App/forms.py
@@ -1,7 +1,16 @@
 from django import forms
 from .models import modelsForm
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+from django.views.generic import CreateView
 
 class formsForm(forms.ModelForm):
     class Meta:
         model= modelsForm
         fields= ["Assunto", "Pergunta", "Resposta"]
+       
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.add_input(Submit('submit', 'Enviar'))
--- Form/App/views.py
+++ Form/App/views.py
@@ -7,13 +7,14 @@ from tablib import Dataset
 from django.shortcuts import render
 from json import loads, dumps
 from .forms import formsForm
-# Create your views here.
+from django.views.generic import CreateView
+
 
 def Form(request):
-    form= formsForm(request.POST or None)
+    form= formsForm(request.POST)
     if form.is_valid():
         form.save()
-  
+
     context= {'form': form }
         
     return render(request, 'Form.html', context)
