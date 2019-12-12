454971ad44193a85389dbcf249b5b94cfb101415
TallesSilva <tallesbugs@hotmail.com>
2019-12-12 16:27:21 -0300

    salvando e removendo arquivos

--- Form/App/models.py
+++ Form/App/models.py
@@ -1,17 +1,29 @@
 from django.db import models
 
-FAQKYROS = 'INFORMAÇÃO DA EMPRESA'
-FAQCONTATO = 'IMFORMAÇÃO DE CONTATO'
-FAQLOCAL = 'INFORMAÇÃO DO LOCAL'
-FAQPRODUTOS = 'INFORMAÇÃO DO PRODUTO'
-CHITCHAT = 'COMUNICAÇÃO FORA DE CONTEXTO'
+ask_faqkyros = 'ask_faq/kyros'
+ask_faqowners_kyros = 'ask_faq/owners_kyros'
+ask_faqfoundation = 'ask_faq/foundation'
+ask_faqservices = 'ask_faq/services'
+ask_faqprojects = 'ask_faq/projects'
+ask_faqproducts = 'ask_faq/products'
+ask_faqkyros_vision = 'ask_faq/kyros_vision'
+ask_faqkyros_values = 'ask_faq/kyros_values'
+ask_faqkyros_address = 'ask_faq/kyros_address'
+ask_faqkyros_contact = 'ask_faq/kyros_contact'
+ask_faqkyros_email = 'ask_faq/kyros_email'
 
 TOPICS = (
-    (FAQKYROS, 'INFORMAÇÃO DA EMPRESA'),
-    (FAQCONTATO, 'IMFORMAÇÃO DE CONTATO'),
-    (FAQLOCAL, 'INFORMAÇÃO DO LOCAL'),
-    (FAQPRODUTOS, 'INFORMAÇÃO DO PRODUTO'),
-    (CHITCHAT, 'COMUNICAÇÃO FORA DE CONTEXTO'),
+    (ask_faqkyros, 'INFORMAÇÃO DA EMPRESA'),
+    (ask_faqowners_kyros, 'INFORMA PROPRIETÁRIOS'),
+    (ask_faqfoundation, 'INFORMA FUNDAÇÃO'),
+    (ask_faqservices, 'INFORMA OS SERVIÇOS'),
+    (ask_faqprojects, 'INFORMA OS PROJETOS'),
+    (ask_faqproducts, 'INFORMA PRODUTOS'),
+    (ask_faqkyros_vision, 'INFORMA VISÃO DA EMPRESA'),
+    (ask_faqkyros_values, 'INFORMA VALORES'),
+    (ask_faqkyros_address, 'INFORMA ENDEREÇO'),
+    (ask_faqkyros_contact, 'INFORMA CONTATO'),
+    (ask_faqkyros_email, 'INFORMA EMAIL'),
         )
 
 class ModeloFormulario(models.Model):
--- Form/App/views.py
+++ Form/App/views.py
@@ -6,6 +6,7 @@ from .models import ModeloFormulario
 from django.views.generic import ListView
 from django.utils import timezone
 
+
 def base(request):
     template = loader.get_template('Dashboard.html')
     context = {
@@ -33,20 +34,23 @@ def Form(request):
 def List(request):
     template = loader.get_template('ListaFormulario.html')
     object_list = ModeloFormulario.objects.all()
+    NLU = open("Rasa/NLU.md", 'w')
+    NLU.write('\n')
+    if request.POST.get('Remove'):
+        formularios = request.POST.getlist('formularios')
+        for formulario in formularios:
+            print(formulario)
+            object_list.filter(id=formulario).delete()
+    elif request.POST.get('Criar'):
+        for objects in object_list:
+            NLU = open("Rasa/NLU.md", 'a')
+            NLU.write('\n')
+            NLU.write('## intent: {}\n' .format(objects.Assunto))
+            NLU.write('- {}\n' .format(objects.Pergunta))
+    object_list = ModeloFormulario.objects.all()
     context = {
         'object_list': object_list,
     }
-    if request.POST.get('Remove'):
-        form = Formulario(request.POST)
-        print(form)
-        if form.is_valid():
-            ModeloFormulario(
-                Checkbox=form.cleaned_data['Checkbox'],
-                Assunto=form.cleaned_data['Assunto'],
-                Pergunta=form.cleaned_data['Pergunta'],
-                Resposta=form.cleaned_data['Resposta']
-            ).save()
-            print('entrou')
     return HttpResponse(template.render(context, request))
 
 
